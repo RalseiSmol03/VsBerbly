c46a0a4ef564cf6a0fd11f0f2f0c1bb709189bae
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-03-22 20:56:21 +0800

    update source/Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -6,12 +6,12 @@ import openfl.utils.AssetType;
 import openfl.utils.Assets as OpenFlAssets;
 import lime.utils.Assets;
 import flixel.FlxSprite;
-#if MODS_ALLOWED
+//#if MODS_ALLOWED
 import sys.io.File;
 import sys.FileSystem;
 import flixel.graphics.FlxGraphic;
 import openfl.display.BitmapData;
-#end
+//#end
 
 import flash.media.Sound;
 
