fbc2d9f15c36f9fec411723e0b1d0a2c967c142b
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-03-22 21:23:12 +0800

    update source/PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1739,9 +1739,9 @@ class PlayState extends MusicBeatState
 
 		var songName:String = Paths.formatToSongPath(SONG.song);
 		var file:String = Paths.json(songName + '/events');
+		/*#if sys
+		if (FileSystem.exists(Paths.modsJson(songName + '/events')) || FileSystem.exists(file)) {*/
 		#if sys
-		if (FileSystem.exists(Paths.modsJson(songName + '/events')) || FileSystem.exists(file)) {
-		#else
 		if (OpenFlAssets.exists(file)) {
 		#end
 			var eventsData:Array<Dynamic> = Song.loadFromJson('events', songName).events;
