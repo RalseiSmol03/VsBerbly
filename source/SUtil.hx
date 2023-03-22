ac4d9c75ed2078261c149eb006052e7b2c477b01
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-03-23 02:27:54 +0800

    update source/SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -7,16 +7,13 @@ import android.widget.Toast;
 import haxe.CallStack;
 import haxe.io.Path;
 import lime.system.System as LimeSystem;
-import lime.utils.Assets as LimeAssets;
+import lime.utils.Log as LimeLogger;
 import openfl.Lib;
 import openfl.events.UncaughtErrorEvent;
-import openfl.system.System as OpenFlSystem;
-import openfl.utils.Assets as OpenFlAssets;
+import openfl.utils.Assets;
 #if sys
 import sys.FileSystem;
 import sys.io.File;
-#else
-import haxe.Log;
 #end
 
 using StringTools;
@@ -61,23 +58,47 @@ class SUtil
 	public static function checkFiles():Void
 	{
 		#if mobile
-		for (file in OpenFlAssets.list().filter(folder -> folder.contains('assets/videos')))
+		if (!FileSystem.exists(SUtil.getStorageDirectory() + 'assets') && !FileSystem.exists(SUtil.getStorageDirectory() + 'mods'))
 		{
-			if (file.endsWith('.mp4'))
+			Lib.application.window.alert("Whoops, seems like you didn't extract the files from the .APK!\nPlease copy the files from the .APK to\n" + SUtil.getStorageDirectory(),
+				'Error!');
+			LimeSystem.exit(1);
+		}
+		else if ((FileSystem.exists(SUtil.getStorageDirectory() + 'assets') && !FileSystem.isDirectory(SUtil.getStorageDirectory() + 'assets'))
+			&& (FileSystem.exists(SUtil.getStorageDirectory() + 'mods') && !FileSystem.isDirectory(SUtil.getStorageDirectory() + 'mods')))
+		{
+			Lib.application.window.alert("Why did you create two files called assets and mods instead of copying the folders from the .APK?, expect a crash.",
+				'Error!');
+			LimeSystem.exit(1);
+		}
+		else
+		{
+			if (!FileSystem.exists(SUtil.getStorageDirectory() + 'assets'))
 			{
-				@:privateAccess
-				for (key => library in LimeAssets.libraryPaths)
-				{
-					var shit:String = file.replace('assets/', '');
-					if (shit.replace(shit.substring(shit.indexOf('/', 0), shit.length), '') == key)
-						SUtil.copyContent('$key:$file', SUtil.getStorageDirectory() + file);
-					else
-						SUtil.copyContent(file, SUtil.getStorageDirectory() + file);
-				}
+				Lib.application.window.alert("Whoops, seems like you didn't extract the assets/assets folder from the .APK!\nPlease copy the assets/assets folder from the .APK to\n" + SUtil.getStorageDirectory(),
+					'Error!');
+				LimeSystem.exit(1);
+			}
+			else if (FileSystem.exists(SUtil.getStorageDirectory() + 'assets') && !FileSystem.isDirectory(SUtil.getStorageDirectory() + 'assets'))
+			{
+				Lib.application.window.alert("Why did you create a file called assets instead of copying the assets directory from the .APK?, expect a crash.",
+					'Error!');
+				LimeSystem.exit(1);
+			}
+
+			if (!FileSystem.exists(SUtil.getStorageDirectory() + 'mods'))
+			{
+				Lib.application.window.alert("Whoops, seems like you didn't extract the assets/mods folder from the .APK!\nPlease copy the assets/mods folder from the .APK to\n" + SUtil.getStorageDirectory(),
+					'Error!');
+				LimeSystem.exit(1);
+			}
+			else if (FileSystem.exists(SUtil.getStorageDirectory() + 'mods') && !FileSystem.isDirectory(SUtil.getStorageDirectory() + 'mods'))
+			{
+				Lib.application.window.alert("Why did you create a file called mods instead of copying the mods directory from the .APK?, expect a crash.",
+					'Error!');
+				LimeSystem.exit(1);
 			}
 		}
-
-		OpenFlSystem.gc(); // clean le memory.
 		#end
 	}
 
@@ -104,7 +125,7 @@ class SUtil
 			switch (stackItem)
 			{
 				case CFunction:
-					stack.push('Non-Haxe (C) Function');
+					stack.push('C Function');
 				case Module(m):
 					stack.push('Module ($m)');
 				case FilePos(s, file, line, column):
@@ -125,12 +146,13 @@ class SUtil
 		#if sys
 		try
 		{
-			if (!FileSystem.exists(SUtil.getStorageDirectory() + 'crash'))
-				FileSystem.createDirectory(SUtil.getStorageDirectory() + 'crash');
+			if (!FileSystem.exists(SUtil.getStorageDirectory() + 'logs'))
+				FileSystem.createDirectory(SUtil.getStorageDirectory() + 'logs');
 
 			File.saveContent(SUtil.getStorageDirectory()
-				+ 'crash/'
-				+ 'DDTO_'
+				+ 'logs/'
+				+ Lib.application.meta.get('file')
+				+ '-'
 				+ Date.now().toString().replace(' ', '-').replace(':', "'")
 				+ '.txt',
 				msg + '\n');
@@ -140,12 +162,12 @@ class SUtil
 			#if android
 			Toast.makeText("Error!\nClouldn't save the crash dump because:\n" + e, Toast.LENGTH_LONG);
 			#else
-			println("Error!\nClouldn't save the crash dump because:\n" + e);
+			LimeLogger.println("Error!\nClouldn't save the crash dump because:\n" + e);
 			#end
 		}
 		#end
 
-		println(msg);
+		LimeLogger.println(msg);
 		Lib.application.window.alert(msg, 'Error!');
 		LimeSystem.exit(1);
 	}
@@ -199,7 +221,7 @@ class SUtil
 			#if android
 			Toast.makeText("Error!\nClouldn't save the file because:\n" + e, Toast.LENGTH_LONG);
 			#else
-			println("Error!\nClouldn't save the file because:\n" + e);
+			LimeLogger.println("Error!\nClouldn't save the file because:\n" + e);
 			#end
 		}
 	}
@@ -208,12 +230,12 @@ class SUtil
 	{
 		try
 		{
-			if (!FileSystem.exists(savePath) && OpenFlAssets.exists(copyPath))
+			if (!FileSystem.exists(savePath) && Assets.exists(copyPath))
 			{
 				if (!FileSystem.exists(Path.directory(savePath)))
 					SUtil.mkDirs(Path.directory(savePath));
 
-				File.saveBytes(savePath, OpenFlAssets.getBytes(copyPath));
+				File.saveBytes(savePath, Assets.getBytes(copyPath));
 			}
 		}
 		catch (e:Dynamic)
@@ -221,18 +243,9 @@ class SUtil
 			#if android
 			Toast.makeText("Error!\nClouldn't copy the file because:\n" + e, Toast.LENGTH_LONG);
 			#else
-			println("Error!\nClouldn't copy the file because:\n" + e);
+			LimeLogger.println("Error!\nClouldn't copy the file because:\n" + e);
 			#end
 		}
 	}
 	#end
-
-	private static function println(msg:String):Void
-	{
-		#if sys
-		Sys.println(msg);
-		#else
-		Log.trace(msg, null); // Pass null to exclude the position.
-		#end
-	}
 }
