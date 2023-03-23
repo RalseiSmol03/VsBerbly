2d6958e0ef60f48cab195b573c6e7f637dcb00a0
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-03-24 03:44:20 +0800

    update source/Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -119,7 +119,7 @@ class Character extends FlxSprite
 
 				var json:CharacterFile = cast Json.parse(rawJson);
 				#if MODS_ALLOWED
-				var txtToFind:String = Paths.getPath('images/' + json.image + '.txt', TEXT);
+				var txtToFind:String = SUtil.getStorageDirectory() + Paths.getPath('images/' + json.image + '.txt', TEXT);
 				if(FileSystem.exists(txtToFind) || Assets.exists(txtToFind))
 				#else
 				if(Assets.exists(Paths.getPath('images/' + json.image + '.txt', TEXT)))
