6c76a8a76352fcb3d99a1f9d3bcd0ec62b336c07
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-03-23 22:03:24 +0800

    update source/FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1461,7 +1461,9 @@ class FunkinLua {
 			luaTrace('musicFadeOut is deprecated! Use soundFadeOut instead.', false, true);
 		});
 
+		#if desktop
 		Discord.DiscordClient.addLuaCallbacks(lua);
+		#end
 
 		call('onCreate', []);
 		#end
