54b09cac86f1ad6dda8015be05c1510542d6a14e
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-03-25 18:28:51 +0800

    update source/options/GraphicsSettingsSubState.hx

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -29,7 +29,7 @@ using StringTools;
 
 class GraphicsSettingsSubState extends BaseOptionsMenu
 {
-	public var persist:Option;
+	public var option:Option;
 
 	public function new()
 	{
@@ -66,13 +66,13 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		option.onChange = onChangeFramerate;
 		#end
 
-		persist = new Option('Persistent Cached Data',
+		option = new Option('Persistent Cached Data',
 			'If checked, images loaded will stay in memory\nuntil the game is closed, this increases memory usage,\nbut basically makes reloading times instant. (Set to true by DEFAULT for some reason.',
 			'imagesPersist',
 			'precache',
 			#if android true #else false #end);
-		persist.onChange = onChangePersistentData; //Persistent Cached Data changes FlxGraphic.defaultPersist
-		addOption(persist);
+		option.onChange = onChangePersistentData; //Persistent Cached Data changes FlxGraphic.defaultPersist
+		addOption(option);
 		super();
 	}
 
