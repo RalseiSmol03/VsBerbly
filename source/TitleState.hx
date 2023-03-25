65516ff3ffcdf4c91108f6d7b04c0df9787e4914
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-03-25 17:51:31 +0800

    update source/TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -209,7 +209,7 @@ class TitleState extends MusicBeatState
 		#end
 		if (ClientPrefs.imagesPersist && precachetemp) {
 			ClientPrefs.imagesPersist = false; // to turn off precache temporarily
-			persist.onChange = onChangePersistentData;
+			GraphicsSettingsSubState.persist.onChange = onChangePersistentData;
 			//trace("Temp Precache On!");
 			precachetemp = false;
 		} else {
@@ -683,7 +683,7 @@ class TitleState extends MusicBeatState
 
 			if (!precachetemp) {
 				ClientPrefs.imagesPersist = true;
-				persist.onChange = onChangePersistentData;
+				GraphicsSettingsSubState.persist.onChange = onChangePersistentData;
 				//trace("Temp Precache On!");
 			}
 		}
